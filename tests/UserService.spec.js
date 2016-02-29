9ece164ad23f1a805c06ae04aaa406d6dd16904f
Todd Motto <todd@toddmotto.com>
2016-02-29 20:44:42 +0000

    chore: Added solution

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,33 @@
 describe('UserService', function () {
+	var $controller;
 
+	beforeEach(module('app'));
+
+	var UserService;
+
+	beforeEach(inject(function ($injector) {
+		UserService = $injector.get('UserService');
+		$httpBackend = $injector.get('$httpBackend');
+
+		$httpBackend.when('GET', '/rest/user').respond({first_name: 'Bill', last_name: 'Gates', email: 'bill@microsoft.com'});
+	}));
+
+	it('should get the current users information', function (done) {
+		$httpBackend.expectGET('/rest/user');
+
+		UserService
+			.getUser()
+			.then(function (res) {
+				var data = res.data;
+				if (data.email === 'bill@microsoft.com' && data.first_name === 'Bill') {
+					done();
+				}
+			});
+
+		$httpBackend.flush();
+	});
+
+	it('should add names together correctly', function () {
+		expect(UserService.createFullName({first_name: 'Test', last_name: 'Test'})).toEqual('Test Test');
+	});
 });
